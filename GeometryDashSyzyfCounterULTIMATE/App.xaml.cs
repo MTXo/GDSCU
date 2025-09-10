fa33216acfe9a3a7ed3c54248a43521c0b716bba
MTXo <109675385+MTXo@users.noreply.github.com>
2025-09-10 15:10:38 +0200

    Update App.xaml.cs

--- GeometryDashSyzyfCounterULTIMATE/App.xaml.cs
+++ GeometryDashSyzyfCounterULTIMATE/App.xaml.cs
@@ -1,4 +1,4 @@
-﻿namespace GeometryDashSyzyfCounterULTIMATE
+namespace GeometryDashSyzyfCounterULTIMATE
 {
     public partial class App : Application
     {
@@ -9,7 +9,7 @@
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new AppShell());
+            return new Window(new MainPage());
         }
     }
 }
